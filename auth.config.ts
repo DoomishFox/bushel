713e7117a0e8c4bd0f91f8fcbbc9b43dee3590e0
Ashely Moon <42523415+DoomishFox@users.noreply.github.com>
2024-03-29 14:11:40 -0700

    fix more environment issues and astro check

--- auth.config.ts
+++ auth.config.ts
@@ -4,13 +4,13 @@ import GitHub from '@auth/core/providers/github'
 export default defineConfig({
 	providers: [
 		GitHub({
-			clientId: import.meta.env.GITHUB_CLIENT_ID,
-			clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
 		}),
 	],
 	callbacks: {
 		async signIn({ user, /*account, profile, email, credentials*/ }) {
-			if (user.id == import.meta.env.GITHUB_ADMIN_USER_ID) {
+			if (user.id == process.env.GITHUB_ADMIN_USER_ID) {
 				return true;
 			}
 			return false;
